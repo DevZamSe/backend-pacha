034b4dd952518c67a282ad9b1bd4af7e8ffd3f6b
luis angel navarro hernandez <luisangelnav12@gmail.com>
2020-12-12 10:52:43 -0500

    ger

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 module.exports.CONFIGURATIONS = {
     DATABASE : {
         MYSQL:{
-        HOST :  "mysql3000.mochahost.com",
-        USER :  "yenko80_pacha",
-        PASSWORD : "Pacha$2020",
-        DATABASE :"yenko80_simbio21"
+            HOST :  "mysql-luisangelnav12.alwaysdata.net",
+            USER :  "209904",
+            PASSWORD : "luisangelnav12",
+            DATABASE :"luisangelnav12_pacha"
         }
     }
 }
@@ -16,8 +16,9 @@ module.exports.CONFIGURATIONS = {
 //          DATABASE :"pacha"
 
 
+// HOST :  "mysql3000.mochahost.com",
+// USER :  "yenko80_pacha",
+// PASSWORD : "Pacha$2020",
+// DATABASE :"yenko80_simbio21"
 
-// HOST :  "mysql-luisangelnav12.alwaysdata.net",
-// USER :  "209904",
-// PASSWORD : "luisangelnav12",
-// DATABASE :"luisangelnav12_pacha"
+
